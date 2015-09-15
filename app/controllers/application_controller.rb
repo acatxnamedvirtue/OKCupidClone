e70a0e18413b993bfb115a3a25c3d5656e45e967
acatxnamedvirtue <tsb252@gmail.com>
2015-09-15 10:34:22 -0400

    Add User and Session Rails MVC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+  def current_user
+    return nil unless session[:token]
+    @current_user ||= User.find_by(session_token: session[:token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    @current_user = user
+    session[:token] = user.reset_token!
+  end
+
+  def logout!
+    current_user.reset_token!
+    session[:token] = nil
+  end
+
+  def require_logged_in!
+    redirect_to new_session_url unless logged_in?
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, :email, :session_token, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username_or_email, password)
+    user = User.find_by(username: username_or_email) ||
+      User.find_by(email: username_or_email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end
